817e732d8a51680867ea3d8b203abcb9187e6974
Sung23092003 <133493285+Sung23092003@users.noreply.github.com>
2024-10-12 21:29:19 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,19 @@ import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
 import carData from "../assets/data/carData";
 import CarItem from "../components/UI/CarItem";
+import Header from '../components/Header/Header.jsx';
+import Slider from '../components/Slider/Slider.jsx';
 
 
 const Home = () => {
   return (
     <Helmet title="Home">
      
-      <section>
-        <Container>
-  <p>Test</p>
-        </Container>
-      </section>
+     <div>
+        <div className="white-gradient"/>
+        <Slider/>
+        {/* <Footer/> */}
+      </div>
     </Helmet>
   );
 };
