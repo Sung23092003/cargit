85179dd889f5c96559797ede5ac4f156732834f8
Sung23092003 <133493285+Sung23092003@users.noreply.github.com>
2024-10-12 21:26:17 +0700

    Update Routers.js

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import CarListing from "../pages/CarListing";
+import CarDetails from "../pages/CarDetails";
 import NotFound from "../pages/NotFound";
 
 const Routers = () => {
@@ -10,6 +11,7 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/cars" element={<CarListing />} />
+      <Route path="/cars/:slug" element={<CarDetails />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
